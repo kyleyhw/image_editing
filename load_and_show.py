18756a7adfe2ed36ffc075df2ac7b4ba26b8e476
Kyle W <kyleyhw@gmail.com>
2025-04-21 01:22:12 +0200

    plotting separate colors works, histogram by color works

--- load_and_show.py
+++ load_and_show.py
@@ -3,73 +3,119 @@ import numpy as np
 import os
 import matplotlib.pyplot as plt
 
-from matplotlib.colors import LinearSegmentedColormap
+import color_settings
+
+colorspace = globals.rgb()
 
 
-
-def load_image(filename):
-    image_directory = './images/'
+def load_image(filename, test=True):
+    image_directory = './images/original/'
+    if test:
+        image_directory = './images/test_images/'
     image_dir = os.path.join(image_directory, filename)
     image = ski.io.imread(image_dir)
     if image.shape[-1] == 4:
         image = ski.color.rgba2rgb(rgba=image)
-    print(image)
     return image
 
-def show_image(image):
-    plt.figure()
-    plt.imshow(image)
-    plt.axis(False)
-    plt.show()
+def show_image(image, ax):
+    ax.imshow(image)
+    ax.set_axis_off()
 
 def separate_rgb(image):
     reordered_axes = np.moveaxis(a=image, source=-1, destination=0)
     color_intensities = tuple(reordered_axes)
     return color_intensities
 
-def plot_separate_colors(image):
-    cmap_maxes = ['#ff0000', '#00ff00', '#0000ff']
-    cmap_names = [f'{color}_to_black' for color in ['red', 'green', 'blue']]
+def plot_separate_colors(image, axs):
+    axs = axs.ravel()
 
-    cmaps = [LinearSegmentedColormap.from_list(name=cmap_names[i], colors=['#000000', cmap_maxes[i]])
-             for i in range(len(cmap_maxes))]
+    color_names = ['red', 'green', 'blue']
+
+
     colors = separate_rgb(image)
 
-    fig, axs = plt.subplots(len(colors)+1, 1)
+
 
     axs[0].imshow(image)
     axs[0].set_axis_off()
+    axs[0].set_title('original')
 
     for i, color in enumerate(colors):
         ax = axs[i+1]
-        ax.imshow(color, cmap=cmaps[i])
+        ax.imshow(color, cmap=colorspace.cmaps[i])
         ax.set_axis_off()
-
-    plt.show()
-
+        ax.set_title(color_names[i])
 
 
+def get_histogram(image, bins=256):
+    counts, bins = np.histogram(image.ravel(), bins=bins)
+    cdf = counts.cumsum()
+    # cdf = cdf / len(counts)
+    return counts, bins, cdf
 
-def show_image_and_color_histogram(image, bins=256):
+def plot_image_and_color_histogram(image, axs, bins=256):
     color_intensities = separate_rgb(image)
 
-    fig, (image_ax, hist_ax) = plt.subplots(2, 1)
+    image_ax, hist_ax = axs
+    cdf_ax = hist_ax.twinx()
+    # exposure_ax = hist_ax.twinx()
 
     image_ax.imshow(image)
     image_ax.set_axis_off()
 
     for i in range(len(color_intensities)):
-        hist_ax.hist(color_intensities[i], bins=bins, histtype='step', label=str(i))
+        color_counts, color_bins, color_cdf = get_histogram(color_intensities[i], bins=bins)
+        hist_ax.stairs(color_counts, color_bins, label=colorspace.color_names[i], color=colorspace.color_hexcodes[i])
+        cdf_ax.plot(color_cdf, color=colorspace.color_hexcodes[i])
 
-    hist_ax.legend()
 
-    plt.show()
+    exposure_counts, exposure_bins, exposure_cdf = get_histogram(image, bins=bins)
+    # exposure_ax.stairs(exposure_counts, exposure_bins, label='exposure', color='black')
+    # exposure_ax.plot(exposure_cdf, color='black')
+
+
+    # hist_ax.legend()
+    # exposure_ax.legend()
+    cdf_ax.set_axis_off()
+
+    hist_ax.set_ylabel('pixel count')
+    hist_ax.set_xlabel('pixel intensity')
+
+def create_and_show_separate_colors(image, show=True, save=None):
+    fig, axs = plt.subplots(2, 2)
+
+    plot_separate_colors(image=image, axs=axs)
+
+    fig.tight_layout()
+
+    if show:
+        plt.show()
+    if save:
+        fig.savefig(fname=f'{save}.png')
+
+def create_and_show_color_histogram(image, show=True, save=None):
+    fig, axs = plt.subplots(2, 1)
+
+    plot_image_and_color_histogram(image=image, axs=axs, bins=256)
+
+    fig
+
+    if show:
+        plt.show()
+    if save:
+        fig.savefig(fname=f'{save}.png')
+
 
 if __name__ == '__main__':
-    image = load_image(filename=('monitor_test_image.jpeg'))
-    # image = load_image(filename='test_image_original.jpeg')
+    # filename = 'monitor_test_original.jpeg'
+    # filename = 'test_image_original.jpeg'
+    # filename = 'rgb_test_original.jpeg'
+    filename = 'climbing_test_original.jpeg'
+    image = load_image(filename=filename)
 
-    plot_separate_colors(image)
+    create_and_show_separate_colors(image=image)
+    create_and_show_color_histogram(image=image)
 
 
 
